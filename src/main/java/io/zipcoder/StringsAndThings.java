9e331120bbbcd86b4342fd26488ba0f95801d021
rjohnson0851 <ron21johnson@comcast.net>
2024-11-03 17:21:22 -0500

    more changes

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -35,21 +35,13 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        String result = "";
-        for (int i = 0; i < base.length(); i++) {
-            if (i > base.length() - remove.length()) {
-                result += base.substring(i, base.length());
-            } else {
-                String substring = base.substring(i, i + remove.length());
-                if (!(substring.equalsIgnoreCase(remove))) {
-                    result += base.charAt(i);
-                }
-            }   else {
-                    i = i + remove.length() - 1;
-              }
-
-        }
-            return result;
+       for (int i = 0; i < remove.length(); i++) {
+           char j = remove.charAt(i);
+           while (base.contains(j+"")){
+               int interate = base.indexOf(j);
+               base = base.replace((j + ""), "");
+           }
+        } return base;
     }
 
     /**
